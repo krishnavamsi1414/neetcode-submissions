4d32c5558d2dc7a2ebaffc0532a3341a45cc8ce2
krishnavamsi1414 <108801133+krishnavamsi1414@users.noreply.github.com>
2026-08-26 13:38:33 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable =9.7
+print(type(variable))
+variable = True
+print(type(variable))
+variable ="kli"
+print(type(variable))
+variable =[]
+print(type(variable))
